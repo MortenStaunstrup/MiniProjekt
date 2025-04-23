3a5dd4f33e073ee95a4aa761fa029b666c10e7dc
MortenStaunstrup <mort490fgromling@gmail.com>
2025-04-23 11:31:34 +0200

    Room Service added

--- MiniProjektGenbrug/Services/Interfaces/IRoomService.cs
+++ MiniProjektGenbrug/Services/Interfaces/IRoomService.cs
@@ -5,5 +5,6 @@ namespace MiniProjektGenbrug.Services.Interfaces;
 public interface IRoomService
 {
     List<Room> GetRooms();
+    Room GetRoomById(int id);
     
 }
--- MiniProjektGenbrug/Services/RoomServiceClient.cs
+++ MiniProjektGenbrug/Services/RoomServiceClient.cs
@@ -1,8 +1,44 @@
+using Core;
 using MiniProjektGenbrug.Services.Interfaces;
 
 namespace MiniProjektGenbrug.Services;
 
 public class RoomServiceClient : IRoomService
 {
-    
+    List<Room> rooms = new List<Room>
+    {
+        new Room
+        {
+            id = 1,
+            Name = "Conference Room A",
+            OpeningHour = new DateTime(2023, 10, 1, 8, 0, 0),
+            ClosingHour = new DateTime(2023, 10, 1, 18, 0, 0),
+            Staffing = 2
+        },
+        new Room
+        {
+            id = 2,
+            Name = "Conference Room B",
+            OpeningHour = new DateTime(2023, 10, 1, 9, 0, 0),
+            ClosingHour = new DateTime(2023, 10, 1, 17, 0, 0),
+            Staffing = 3
+        },
+        new Room
+        {
+            id = 3,
+            Name = "Meeting Room A",
+            OpeningHour = new DateTime(2023, 10, 1, 7, 30, 0),
+            ClosingHour = new DateTime(2023, 10, 1, 19, 0, 0),
+            Staffing = 1
+        }
+    };
+    public List<Room> GetRooms()
+    {
+        return rooms;
+    }
+
+    public Room GetRoomById(int id)
+    {
+        return rooms.FirstOrDefault(x => x.id == id);
+    }
 }
